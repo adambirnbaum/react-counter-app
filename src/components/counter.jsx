b0c35c04c57e15fb17c08c24d87331984c894bef
Adam Birnbaum <birnbaum.adam@gmail.com>
2018-10-03 10:40:12 -0500

    update test message

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,15 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <React.Fragment>
+        {" "}
+        {/* use this instead of adding a <div>*/}
+        <h1>Hello World</h1>
+        <button>Increment</button>
+      </React.Fragment>
+    );
   }
 }
 
