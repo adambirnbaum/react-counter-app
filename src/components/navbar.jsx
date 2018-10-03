afbcbd3fd79e9d73f059c0584a0c755e4d3b4a79
Adam Birnbaum <birnbaum.adam@gmail.com>
2018-10-03 18:02:38 -0500

    add in lifecycle hooks

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 
-class Navbar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="#">
-          Navbar {""}
-          <span className="badge badge-pill badge-secondary">
-            {this.props.totalCounters}
-          </span>
-        </a>
-      </nav>
-    );
-  }
-}
+// Stateless functional component
+const Navbar = ({ totalCounters }) => {
+  console.log("Navbar - rendered");
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar {""}
+        <span className="badge badge-pill badge-secondary">
+          {totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
 
 export default Navbar;
